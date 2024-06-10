61d80023d8044823e709cad75c1a2d201d4fc725
Homo Erectus <125422933+NotHomoSapien@users.noreply.github.com>
2024-06-10 16:38:54 -0400

    merge

--- scripts/password.js
+++ scripts/password.js
@@ -5,6 +5,19 @@ const password3 = "p3";
 
 var innerDiv = document.getElementById('login-form-1')
 
+<<<<<<< HEAD
+=======
+document.getElementById("login-form-1").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputPassword = document.getElementById("password-1").value;
+    if (inputPassword === password1) {
+        alert("Here is a letter you should remember: H");
+       selectLanguage(languages[1])
+    } else {
+        alert("Incorrect password!");
+    }
+});
+>>>>>>> parent of dac32f3 (d)
 
 innerDiv.addEventListener('submit', function(event) {
     event.preventDefault();
