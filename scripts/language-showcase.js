92855ad3adcc0e09babdb222ebba373ff7a5733e
Homo Erectus <125422933+NotHomoSapien@users.noreply.github.com>
2024-06-10 09:53:39 -0400

    d

--- scripts/language-showcase.js
+++ scripts/language-showcase.js
@@ -40,8 +40,8 @@ function setLanguageInfo(language) {
        <h1 class="container-h1">Enter what you've found here...</h1>
           <form action="#">
             <input type="text" placeholder="Enter Password" id="password-1">
-            <a class="button1"><span
-              class="button1span"><button type="submit">Test your fate</button></span></a>
+            <button type="submit"><a class="button1"><span
+              class="button1span">Test your fate</span></a></button>
           </form>
         </div>
       `;
