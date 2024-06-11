f8bfea0ba1e59721362e4672a0b54fbbf50937ce
Homo Erectus <125422933+NotHomoSapien@users.noreply.github.com>
2024-06-11 18:51:35 -0400

    Update tabs.js

--- js/tabs.js
+++ js/tabs.js
@@ -43,15 +43,15 @@ function setLanguageInfo(language) {
 
     case "Inception":
       languageInfoBox.innerHTML = `
-      <div class = "form-fields-text">
+      <div class="form-fields-text">
           <p class="language-description">
             Throughout your journey you will face many tricks and twists, be prepared to go through and escape. What is your mothers mother?
           </p>
           <div class="container">
-       <h1 class="container-h1">Enter what you've found here...</h1>
+            <h1 class="container-h1">Enter what you've found here...</h1>
             <input type="text" placeholder="Enter Password" id="${passwordInputId}">
             <button type="button" class="button11" id="${buttonId}">Test your fate</button>
-        </div>
+          </div>
       </div>
       `;
       document.getElementById(buttonId).addEventListener("click", function (event){
@@ -70,15 +70,15 @@ function setLanguageInfo(language) {
 
     case "King of Skies":
       languageInfoBox.innerHTML = `
-      <div class = "form-fields-text" style = "display: none;">
+      <div class="form-fields-text">
           <p class="language-description">
             Have you had enough yet??<br>No? Well I'm sure you will quit now. Refer back to the page you were sent to... Thats all I can give you for now.
           </p>
           <div class="container">
-       <h1 class="container-h1">Enter what you've found here...</h1>
+            <h1 class="container-h1">Enter what you've found here...</h1>
             <input type="text" placeholder="Enter Password" id="${passwordInputId}">
             <button type="button" class="button11" id="${buttonId}">Test your fate</button>
-        </div>
+          </div>
       </div>
       `;
       document.getElementById(buttonId).addEventListener("click", function (event){
@@ -97,32 +97,32 @@ function setLanguageInfo(language) {
 
     case "Worst Nightmare":
       languageInfoBox.innerHTML = `
-          <div class = "form-fields-text" style = "display: none;">
-              <p class="language-description">
-                Throughout your journey you will face many tricks and twists, be prepared to go through and escape. What is your mothers mother?
-              </p>
-              <div class="container">
-           <h1 class="container-h1">Enter what you've found here...</h1>
-                <input type="text" placeholder="Enter Password" id="${passwordInputId}">
-                <button type="button" class="button11" id="${buttonId}">Test your fate</button>
-              </div>
-          </div>
-        `;
-        document.getElementById(buttonId).addEventListener("click", function (event){
-          event.preventDefault();
-          const inputPassword = document.getElementById(passwordInputId).value;
-          if (inputPassword === password3) { 
-            if (currentLanguageIndex < languages.length - 1) {
-              currentLanguageIndex++;
-              languages[currentLanguageIndex].style.display = 'block';
-            }
-          } else {
-            alert("Incorrect password!");
+        <div class="form-fields-text">
+            <p class="language-description">
+              Throughout your journey you will face many tricks and twists, be prepared to go through and escape. What is your mothers mother?
+            </p>
+            <div class="container">
+              <h1 class="container-h1">Enter what you've found here...</h1>
+              <input type="text" placeholder="Enter Password" id="${passwordInputId}">
+              <button type="button" class="button11" id="${buttonId}">Test your fate</button>
+            </div>
+        </div>
+      `;
+      document.getElementById(buttonId).addEventListener("click", function (event){
+        event.preventDefault();
+        const inputPassword = document.getElementById(passwordInputId).value;
+        if (inputPassword === password3) { 
+          if (currentLanguageIndex < languages.length - 1) {
+            currentLanguageIndex++;
+            languages[currentLanguageIndex].style.display = 'block';
           }
-        });
-        break;
-    }
+        } else {
+          alert("Incorrect password!");
+        }
+      });
+      break;
   }
+}
   
   // Add event listeners to each language tab
   languages.forEach((language, index) => {
